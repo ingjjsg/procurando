8dd6dc91e0f69e93d6d1bef54d546ecbe166b4e5
ingjjsg <ingjjsg@gmail.com>
2012-09-20 17:38:26 +0000

    corrigiendo corrigiendo en el controlador de reporte agenda

--- procurando/controlador/reporteAgendaControlador.php
+++ procurando/controlador/reporteAgendaControlador.php
@@ -1,20 +1,12 @@
 <?php
 
-require_once '../modelo/ctblagendaModelo.php';
-
 function selectAgendaReporte($request,$tipoReporte){
         $respuesta= new xajaxResponse();
-        $proagenda= new clTblagenda();
-        $data= "";
-        $data= $proagenda->selectAgendaReporte($request['id_tipo'], $request['id_evento'], $request['id_prioridad'],$request['id_estado'],$request['id_recordatorio'],$request['id_unidad'],$request['id_refiere'],$request['id_tipo_organismo'],$request['id_organismo']);
-        
-        if($data){
-            $compactada=urlencode(serialize($data));
-            if($tipoReporte == 'pdf'){
-                $respuesta->script("location.href='../reportes/reporte_agenda_pdf.php?data=".$compactada."'");
-            }else if($tipoReporte == 'ods'){
-                $respuesta->script("location.href='../reportes/reporte_agenda_ods.php?data=".$compactada."'");
-            }
+
+        if($tipoReporte == 'pdf'){
+            $respuesta->script("location.href='../reportes/reporte_agenda_pdf.php?data=".$request."'");
+        }else if($tipoReporte == 'ods'){
+            $respuesta->script("location.href='../reportes/reporte_agenda_ods.php?data=".$request."'");
         }else{
             $respuesta->alert("No Hay Datos");
         }     
