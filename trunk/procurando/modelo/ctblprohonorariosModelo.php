44e24e6cf349400f13535b090327966f49983582
ingjjsg@gmail.com <ingjjsg@gmail.com@1df858e6-029c-ba91-b5de-87a9a7f5e24a>
2012-10-11 12:52:06 +0000

    cambiando nombre a los reportes

--- trunk/procurando/modelo/ctblprohonorariosModelo.php
+++ trunk/procurando/modelo/ctblprohonorariosModelo.php
@@ -121,7 +121,7 @@ class cltblprohonorariosModelo {
     public function selectAllHonorariosCargados(){
         $conn= new Conexion();
         $conn->abrirConexion();
-        $sql.= "SELECT * FROM ".clConstantesModelo::correspondencia_table."vista_honorarios_cargados ";
+        $sql.= "SELECT * FROM ".clConstantesModelo::correspondencia_table."vista_honorarios_cargados where modulo='OAS' ";
         $conn->sql= $sql;
         $data= $conn->ejecutarSentencia(2);
         $conn->cerrarConexion();        
@@ -159,7 +159,7 @@ class cltblprohonorariosModelo {
         $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tipo) AS id_tipo_honorario ";
         $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tramite) AS id_tramite_honorario ";
         $sql.=" ,(SELECT intprecio FROM ".clConstantesModelo::correspondencia_table."tblunidadtributaria WHERE id_unidad= id_unidad) AS id_unidad_honorario ";
-        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios order by id_honorarios asc";
+        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios where modulo='OAS' order by id_honorarios asc";
         $conn->sql= $sql;
         $data= $conn->ejecutarSentencia(2);
         $conn->cerrarConexion();        
@@ -175,7 +175,7 @@ class cltblprohonorariosModelo {
         $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tipo) AS id_tipo_honorario ";
         $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tramite) AS id_tramite_honorario ";
         $sql.=" ,(SELECT intprecio FROM ".clConstantesModelo::correspondencia_table."tblunidadtributaria WHERE id_unidad= id_unidad limit 1) AS id_unidad_honorario ";
-        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios  Where id_honorarios>0 ";
+        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios  Where id_honorarios>0 and modulo='OAS'";
         if ($fil_id_tipo!='0')
            $sql.=" and id_tipo=".$fil_id_tipo."";
         if ($fil_id_tramite!='0') 
@@ -236,6 +236,67 @@ class cltblprohonorariosModelo {
         $conn->cerrarConexion();
         return $retorno;
     }
+    
+    
+     public function insertHonorarioLitigio(){
+        $conn= new Conexion();
+        $conn->abrirConexion();
+        $sql= "INSERT INTO ".clConstantesModelo::correspondencia_table."tblprohonorarios (id_tipo, id_tramite, id_unidad, numunidad,modulo) values ";
+        $sql.= "(".$this->getId_tipo().",".$this->getId_tramite().",".$this->getId_unidad().",".$this->getNumunidad().",'LTG')";
+        $conn->sql=$sql;
+        $conn->ejecutarSentencia();
+        $retorno= $this->verIdHonorario();
+        $conn->cerrarConexion();
+        return $retorno;
+    }
+    
+    public function selectAllHonorariosLitigio(){
+        $conn= new Conexion();
+        $conn->abrirConexion();
+        $sql.= "SELECT id_honorarios, id_tipo, id_tramite, id_unidad, numunidad ";
+        $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tipo) AS id_tipo_honorario ";
+        $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tramite) AS id_tramite_honorario ";
+        $sql.=" ,(SELECT intprecio FROM ".clConstantesModelo::correspondencia_table."tblunidadtributaria WHERE id_unidad= id_unidad) AS id_unidad_honorario ";
+        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios where modulo='LTG' order by id_honorarios asc";
+        $conn->sql= $sql;
+        $data= $conn->ejecutarSentencia(2);
+        $conn->cerrarConexion();        
+        return $data;
+    }     
+    
+
+    
+    public function selectFiltrarHonorariosLitigio($fil_id_tipo, $fil_id_tramite, $fil_id_unidad){
+        $conn= new Conexion();
+        $conn->abrirConexion();
+        $sql.= "SELECT id_honorarios, id_tipo, id_tramite, id_unidad, numunidad ";
+        $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tipo) AS id_tipo_honorario ";
+        $sql.=" ,(SELECT stritema FROM ".clConstantesModelo::correspondencia_table."tblmaestros WHERE id_maestro= id_tramite) AS id_tramite_honorario ";
+        $sql.=" ,(SELECT intprecio FROM ".clConstantesModelo::correspondencia_table."tblunidadtributaria WHERE id_unidad= id_unidad limit 1) AS id_unidad_honorario ";
+        $sql.=" from ".clConstantesModelo::correspondencia_table."tblprohonorarios  Where id_honorarios>0 and modulo='LTG'";
+        if ($fil_id_tipo!='0')
+           $sql.=" and id_tipo=".$fil_id_tipo."";
+        if ($fil_id_tramite!='0') 
+           $sql.=" and id_tramite=".$fil_id_tramite."";
+        if ($fil_id_unidad!='0')
+           $sql.=" and id_unidad=".$fil_id_unidad."";
+//        exit($sql);      
+        $sql.=" order by id_honorarios asc";
+        $conn->sql= $sql;
+        $data= $conn->ejecutarSentencia(2);
+        $conn->cerrarConexion();        
+        return $data;
+    }
+    
+    public function selectAllHonorariosCargadosLitigio(){
+        $conn= new Conexion();
+        $conn->abrirConexion();
+        $sql.= "SELECT * FROM ".clConstantesModelo::correspondencia_table."vista_honorarios_cargados where modulo='LTG'";
+        $conn->sql= $sql;
+        $data= $conn->ejecutarSentencia(2);
+        $conn->cerrarConexion();        
+        return $data;
+    } 
     
     
 
